bbe97636e8228a22bf1202666dd1ec5a88648b60
shallaNik <43818681+shallaNik@users.noreply.github.com>
2020-09-06 10:43:53 +0530

    initial commit

---
+++ calendar.py
@@ -0,0 +1,11 @@
+# Program to display calendar of the given month and year
+
+# importing calendar module
+import calendar
+
+yy = 2020  # year
+mm = 8    # month
+
+# display the calendar
+print("Here's your calender")
+print(calendar.month(yy, mm))
